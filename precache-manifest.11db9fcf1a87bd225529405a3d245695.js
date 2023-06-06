cf6655920c65f08ae608e62caa618f90c73dc171
egarza27 <elizabethigarza10@gmail.com>
2023-06-06 18:36:39 -0500

    Updates

---
+++ precache-manifest.11db9fcf1a87bd225529405a3d245695.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d855dcd248dfba8a4c7f5e91880b66a1",
+    "url": "/JS411_Protected_Routes/index.html"
+  },
+  {
+    "revision": "bef8783bec1bdc9eea58",
+    "url": "/JS411_Protected_Routes/static/css/main.d5793637.chunk.css"
+  },
+  {
+    "revision": "2cc0f4c58ed88a49cb63",
+    "url": "/JS411_Protected_Routes/static/js/2.60ccbc92.chunk.js"
+  },
+  {
+    "revision": "bef8783bec1bdc9eea58",
+    "url": "/JS411_Protected_Routes/static/js/main.72a6466e.chunk.js"
+  },
+  {
+    "revision": "62fab8e835d0df3eac88",
+    "url": "/JS411_Protected_Routes/static/js/runtime~main.e56bea01.js"
+  }
+]);
